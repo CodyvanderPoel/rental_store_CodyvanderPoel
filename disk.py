5ec5f01b550a78e62957c26bcc913551151151cd
basecamp <cvanderpoel@basecampcodingacademy.org>
2018-07-24 15:01:54 -0500

    I have made my functions that read the files properly and I am fixing to work on my function that will write to the history txt file.

--- disk.py
+++ disk.py
@@ -3,10 +3,16 @@ def parse_inventory_item(string):
     return [name, int(stock), int(replacement_fee), int(rental_fee)]
 
 
+def which_file_to_open(choice):
+    if choice == "U":
+        inventory = open_file('inventory.txt')
+        return inventory
+
+
 def open_file(file_name):
+    # This was my first attempt at this
     # with open(file_name) as file:
     #     contents = {file.readlines()}
-    #     print(contents)
     #     return contents
     with open(file_name) as file:
         string = file.read()
@@ -17,21 +23,11 @@ def open_file(file_name):
                 d = parse_inventory_item(line)
                 inventory[d[0]] = {
                     'Stock': d[1],
-                    'Replacement Fee': d[2],
-                    'Rental Fee': d[3]
+                    'Replacement_fee': d[2],
+                    'Rental_fee': d[3]
                 }
         return inventory
 
 
-def which_file_to_open(choice):
-    if choice == "U":
-        inventory = open_file('inventory.txt')
-        return inventory
-    else:
-        history = open_file('history.txt')
-        stock = open_file('inventory.txt')
-        return history, stock
-
-
-def reduce_value(inventory):
+def write_to_file():
     pass
