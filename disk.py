e7a1a9cecb38809c8e5d98edcfaa0c496435d5e0
basecamp <cvanderpoel@basecampcodingacademy.org>
2018-07-25 15:11:46 -0500

    I made it to where an employee could view the transaction history

--- disk.py
+++ disk.py
@@ -3,12 +3,17 @@ def parse_inventory_item(string):
     return [name, int(stock), int(replacement_fee), int(rental_fee)]
 
 
-def open_file(file_name):
+def parse_history_item(string):
+    time, days_rented, deposit, profit = string.split(',')
+    return [time, days_rented, deposit, profit]
+
+
+def open_file_inv():
     # This was my first attempt at this
     # with open(file_name) as file:
     #     contents = {file.readlines()}
     #     return contents
-    with open(file_name) as file:
+    with open('inventory.txt') as file:
         string = file.read()
         inventory = {}
         lines = string.split('\n')[1:]
@@ -23,5 +28,53 @@ def open_file(file_name):
         return inventory
 
 
-def write_to_file():
-    pass
+def open_file_history():
+    with open('history.txt') as file:
+        string = file.read()
+        lines = string.split('\n')[1:]
+        history = {}
+        for line in lines:
+            if line:
+                d = parse_history_item(line)
+                history[d[0]] = {
+                    'Days Rented': d[1],
+                    'Deposit': d[2],
+                    'Profit': d[3]
+                }
+        return history
+
+
+def inventory_to_string(inventory):
+    st = ''
+    for key in inventory:
+        item = inventory[key]
+        st += '{},{},{},{}\n'.format(
+            key,
+            item['Stock'],
+            item['Replacement_fee'],
+            item['Rental_fee'],
+        )
+
+    return st.rstrip()
+
+
+def history_to_string(history):
+    st = ''
+    item = history
+    st += '\n,{},{},{},{}'.format(item['time'], item['days_rented'],
+                                  item['deposit'], item['profit'])
+    return st.rstrip()
+
+
+def write_to_file_inv(inventory):
+    with open('inventory.txt', 'w') as file:
+        file.write(
+            f'name,stock,replacement_fee,daily_fee\n{inventory_to_string(inventory)}'
+        )
+
+
+def write_to_file_history(history):
+    with open('history.txt', 'w') as file:
+        file.write(
+            f'name,time,days_rented,deposit,profit{history_to_string(history)}'
+        )
