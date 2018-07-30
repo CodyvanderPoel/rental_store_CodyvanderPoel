50ce65217eda0f978748b2717e38bf31981772e0
basecamp <cvanderpoel@basecampcodingacademy.org>
2018-07-30 15:56:25 -0500

    I finished testing my disk

--- test_disk.py
+++ test_disk.py
@@ -75,3 +75,22 @@ def test_history_to_string():
     result = history_to_string(history)
 
     assert result == '\n2018-07-30 10:57:38.919233,7,1000.0,127.33'
+
+
+@fake_file({
+    'inventory.txt':
+    'number,name,stock,replacement_fee,daily_fee1,Sedans,10,5000,12'
+})
+def test_write_to_file_inv():
+    write_to_file_inv(inventory)
+
+    assert open('inventory.txt').read(
+    ) == 'number,name,stock,replacement_fee,daily_fee\n1,Sedans,10,5000,12'
+
+
+@fake_file({'history.txt': 'time,days_rented,deposit,profit'})
+def test_write_to_file_history():
+    write_to_file_history(history)
+
+    assert open('history.txt').read(
+    ) == 'time,days_rented,deposit,profit\n2018-07-30 10:57:38.919233,7,1000.0,127.33'
