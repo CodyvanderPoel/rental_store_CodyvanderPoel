f4afe86eef194ad8bf973dc11543ce59094c49ba
basecamp <cvanderpoel@basecampcodingacademy.org>
2018-08-06 15:11:31 -0500

    I have tested my login function

--- test_shell.py
+++ test_shell.py
@@ -3,6 +3,16 @@ from bcca.test import with_inputs, should_print
 
 
 @with_inputs('c')
-@should_print()
-def test_customer_login():
-    pass
+@should_print
+def test_customer_login(output):
+    result = login()
+
+    assert "c" in result
+
+
+@with_inputs('e')
+@should_print
+def test_employee_login(output):
+    result = login()
+
+    assert "e" in result
